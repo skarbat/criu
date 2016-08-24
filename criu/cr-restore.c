803049c8a194dfb5279f80d9e5f89f3df0417e65
Cyrill Gorcunov <gorcunov@openvz.org>
2016-08-24 13:32:27 +0300

    pstree: Make restore_finish_stage helper to use holder variable
    The @task_entries variable is declared as external in
    criu/include/pstree.h and restore_finish_stage refers
    to it implicitly. This is a bad habbit because the name
    get propagated into pie code where we have a local
    static variable, which eventually may shadow the former
    copy with gcc 4.9 series as been reported by Nikolay.
    
    Lets make restore_finish_stage helper to take variable
    name explicitly.
    
    Reported-by: Nikolay Borisov <kernel@kyup.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Reviewed-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -816,7 +816,7 @@ static int restore_one_zombie(CoreEntry *core)
 	prctl(PR_SET_NAME, (long)(void *)core->tc->comm, 0, 0, 0);
 
 	if (task_entries != NULL) {
-		restore_finish_stage(CR_STATE_RESTORE);
+		restore_finish_stage(task_entries, CR_STATE_RESTORE);
 		zombie_prepare_signals();
 	}
 
@@ -898,7 +898,7 @@ static int restore_one_task(int pid, CoreEntry *core)
 			return -1;
 		}
 
-		restore_finish_stage(CR_STATE_RESTORE);
+		restore_finish_stage(task_entries, CR_STATE_RESTORE);
 		if (wait_on_helpers_zombies()) {
 			pr_err("failed to wait on helpers and zombies\n");
 			ret = -1;
@@ -1390,7 +1390,7 @@ static int restore_task_with_children(void *_arg)
 
 	/* Wait prepare_userns */
 	if (current->parent == NULL &&
-            restore_finish_stage(CR_STATE_RESTORE_NS) < 0)
+            restore_finish_stage(task_entries, CR_STATE_RESTORE_NS) < 0)
 			goto err;
 
 	/*
@@ -1426,7 +1426,7 @@ static int restore_task_with_children(void *_arg)
 		if (root_prepare_shared())
 			goto err;
 
-		if (restore_finish_stage(CR_STATE_RESTORE_SHARED) < 0)
+		if (restore_finish_stage(task_entries, CR_STATE_RESTORE_SHARED) < 0)
 			goto err;
 	}
 
@@ -1463,7 +1463,7 @@ static int restore_task_with_children(void *_arg)
 		fini_restore_mntns();
 	}
 
-	if (restore_finish_stage(CR_STATE_FORKING) < 0)
+	if (restore_finish_stage(task_entries, CR_STATE_FORKING) < 0)
 		goto err;
 
 	if (restore_one_task(current->pid.virt, ca->core))
